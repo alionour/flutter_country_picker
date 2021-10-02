4659592eaff2d65fd7b8ae974fc380bd1387c9c6
ali <alionour22@gmail.com>
2021-10-02 14:08:45 +0200

    update diacritic

--- test/flutter_country_picker_test.dart
+++ test/flutter_country_picker_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_country_picker/flutter_country_picker.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_country_picker');
